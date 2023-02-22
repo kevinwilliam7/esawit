1ac48fb395bbf31b7177d4fcbb4b855669fa347f
Thomas Adi Putra <thomasadiputra@gmail.com>
2023-02-22 22:07:55 +0700

    Add perkebunan controller for admin

---
+++ app/Http/Controllers/Perkebunan/PerkebunanController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Perkebunan;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Perkebunan\StorePerkebunanRequest;
+use App\Models\Perkebunan\Perkebunan;
+use Illuminate\Http\Request;
+
+class PerkebunanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $perkebunans = Perkebunan::select('id', 'nama', 'npwp', 'alamat', 'pola_kemitraan')->get();
+        return view('perkebunan.index', compact('perkebunans'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('perkebunan.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  app/Http/Requests/Perkebunan/StorePerkebunanRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePerkebunanRequest $request)
+    {
+        try {
+            Perkebunan::create($request->all());
+        } catch (\Throwable $th) {
+            //throw $th;
+            dd($th);
+        }
+        return redirect()->route('perkebunan.index')->with('success', 'Berhasil menambah data perkebunan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  app/Models/Perkebunan/Perkebunan  $perkebunan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Perkebunan $perkebunan)
+    {
+        // $perkebunan->delete();
+        return response('', 500);
+    }
+}
