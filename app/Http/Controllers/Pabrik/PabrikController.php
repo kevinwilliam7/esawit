b0c1075b1d81c85e0c741b09df492e81f9dc3498
Thomas Adi Putra <thomasadiputra@gmail.com>
2023-02-23 22:36:45 +0700

    Create Pabrik model, controller, and migration

---
+++ app/Http/Controllers/Pabrik/PabrikController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Pabrik;
+
+use DataTables;
+use Illuminate\Http\Request;
+use App\Models\Pabrik\Pabrik;
+use App\Http\Controllers\Controller;
+
+class PabrikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $pabriks = Pabrik::select('nama', 'npwp', 'nama_grup', 'kapasitas_produksi');
+            return DataTables::of($pabriks)->addIndexColumn()->make();
+        }
+        return view('pabrik.admin.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pabrik.admin.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
