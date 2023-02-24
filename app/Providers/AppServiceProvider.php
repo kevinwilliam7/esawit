58c3fadcb0465ce74161590b415e430a197cfe1d
Thomas <thomasadiputra@gmail.com>
2023-02-24 15:24:35 +0700

    Add Perkebunan observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Perkebunan\Perkebunan;
+use App\Observers\Perkebunan\PerkebunanObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Perkebunan::observe(PerkebunanObserver::class);
     }
 }
