6e14c169f82a0dab8e03870ffca2bbd244d36f17
Thomas Adi Putra <thomasadiputra@gmail.com>
2023-02-28 21:11:37 +0700

    Add content tilte & action column

--- resources/views/pabrik/admin/index.blade.php
+++ resources/views/pabrik/admin/index.blade.php
@@ -10,13 +10,15 @@
     </style>
 @endsection
 
+@section('content-title', 'Pabrik')
+
 @section('content')
     <div class="container">
         <div class="card">
             <div class="card-header">
                 <div class="card-title">
                     <b>
-                        <h5>Data Perusahaan Perkebunan</h5>
+                        <h5>Data Pabrik</h5>
                     </b>
                 </div>
             </div>
@@ -44,27 +46,42 @@
     </div>
 @endsection
 
-
 @section('js')
     <script src="{{ asset('assets/libs/DataTables/datatables.min.js') }}"></script>
     <script>
-        $(document).ready(function() {
+        $(document).ready(function() {           
             const table = $('table#pabriks').DataTable({
                 'scrollX': true,
                 'processing': true,
                 'serverSide': true,
-                'ajax': '{{ route("admin.pabrik.index") }}',
-                'columns': [
-                  { 'data': 'DT_RowIndex', 'name': 'DT_RowIndex', 'orderable': false, 'searchable': false },
-                  { 'data': 'nama', 'name': 'nama' },
-                  { 'data': 'npwp', 'name': 'npwp' },
-                  { 'data': 'nama_grup', 'name': 'nama_grup' },
-                  { 'data': 'kapasitas_produksi', 'name': 'kapasitas_produksi' },
-                  { 
-                    'data': 'nama', 
-                    'name': 'nama', 
-                    'render': function(data) {
-                      return `
+                'ajax': '{{ route('admin.pabrik.index') }}',
+                'columns': [{
+                        'data': 'DT_RowIndex',
+                        'name': 'DT_RowIndex',
+                        'orderable': false,
+                        'searchable': false
+                    },
+                    {
+                        'data': 'nama',
+                        'name': 'nama'
+                    },
+                    {
+                        'data': 'npwp',
+                        'name': 'npwp'
+                    },
+                    {
+                        'data': 'nama_grup',
+                        'name': 'nama_grup'
+                    },
+                    {
+                        'data': 'kapasitas_produksi',
+                        'name': 'kapasitas_produksi'
+                    },
+                    {
+                        'data': 'action',
+                        'name': 'action',
+                        'render': function(data) {
+                            return `
                         <a href="{{ route('admin.pabrik.index') }}/${data}" 
                           class="btn btn-sm btn-primary btn-bordered waves-effect waves-light me-1 mb-1 rounded-2">
                           <i class="fa fa-eye"></i> Lihat
@@ -78,8 +95,8 @@
                           <i class="fa fa-trash-o"></i> Hapus Data
                         </button>
                       `;
-                    }
-                  },
+                        }
+                    },
                 ],
                 'columnDefs': [{
                         'target': '_all',
@@ -92,17 +109,20 @@
                         'searchable': false,
                         'width': 520
                     },
-                    { 'target': [0], 'className': 'dt-center' },
+                    {
+                        'target': [0],
+                        'className': 'dt-center'
+                    },
                 ],
             });
 
             $('table').on('click', 'button.btn-danger', async function() {
                 var tr = $(this).closest('tr');
                 var row = table.row(tr);
-                var nama = row.data().nama;
+                var action = row.data().action;
                 if (await confirmation()) {
                     $.ajax({
-                        'url': `{{ url('admin/pabrik') }}/${nama}`,
+                        'url': `{{ url('admin/pabrik') }}/${action}`,
                         'method': 'POST',
                         'data': {
                             '_token': '{{ csrf_token() }}',
@@ -116,7 +136,6 @@
                             table.ajax.reload();
                         };
                     }).catch((error) => {
-                        // console.log(error);
                         swal('Gagal menghapus data', 'error');
                     });
                 };
