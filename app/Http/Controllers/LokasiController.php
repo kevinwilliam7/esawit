3862238cf0abaf2952ac5fa347b31769b301c2dd
Thomas Adi Putra <thomasadiputra@gmail.com>
2023-02-28 21:05:31 +0700

    Create LokasiController

---
+++ app/Http/Controllers/LokasiController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lokasi\Desa;
+use App\Models\Lokasi\Kecamatan;
+use Illuminate\Http\Request;
+
+class LokasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function kecamatan($kabupaten)
+    {
+        return response()->json([
+            'data' => Kecamatan::select('id', 'name')->where('kabupaten_id', $kabupaten)->get()
+        ]);
+    }
+
+    public function desa($kecamatan)
+    {
+        return response()->json([
+            'data' => Desa::select('id', 'name')->where('kecamatan_id', $kecamatan)->get()
+        ]);
+    }
+}
