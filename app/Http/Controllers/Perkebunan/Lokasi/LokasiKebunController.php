5c3585a4a29c357b478715c7a6487b70b9270a22
Thomas <thomasadiputra@gmail.com>
2023-03-03 17:16:26 +0700

    Add controller for perkebunan detail

---
+++ app/Http/Controllers/Perkebunan/Lokasi/LokasiKebunController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Perkebunan\Lokasi;
+
+use App\Http\Controllers\Controller;
+use App\Models\Perkebunan\Perkebunan;
+use Illuminate\Http\Request;
+
+class LokasiKebunController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param App\Models\Perkebunan\Perkebunan  $perkebunan
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Perkebunan $perkebunan)
+    {
+        $perkebunan->lokasis()->create($request->all());
+        return redirect()->back()->with('success', 'Berhasil menambah data lokasi kebun');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  App/Models/Perkebunan/Perkebunan  $perkebunan
+     * @param  string  $nama
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Perkebunan $perkebunan, string $nama, int $id)
+    {
+        $perkebunan->lokasis->find($id)->update($request->all());
+        return redirect()->back()->with('success', 'Berhasil mengubah data lokasi kebun');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  App\Models\Perkeebunan\Perkebunan  $perkebunan
+     * @param  string nama
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Perkebunan $perkebunan, string $nama, int $id)
+    {
+        $perkebunan->lokasis->find($id)->delete();
+        return redirect()->back()->with('success', 'Berhasil menghapus data lokasi kebun');
+    }
+}
